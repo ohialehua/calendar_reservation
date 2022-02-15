0e237b09e1946b6cd7210fd853478bfa1bca792e
ohialehua <kairyu.bonvivant@gmail.com>
2022-02-15 04:23:18 +0000

    [Add devise]

--- Gemfile
+++ Gemfile
@@ -61,3 +61,11 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem 'devise'
+
+gem 'bootstrap', '~> 4.5'
+gem 'simple_calendar', '~> 2.0'
+gem 'font-awesome-sass', '~> 5.13'
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users,skip: [:passwords,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+namespace :admin do
+  root to: 'homes#top'
+end
+
+scope module: :public do
+  root to: 'homes#top'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
